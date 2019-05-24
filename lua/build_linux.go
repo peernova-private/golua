998e68e712f370401dd4bf866d5c317372a994ed
Christopher Krowl <chrisk@peernova.com>
2019-05-24 14:34:01 -0700

    Remove relative path

--- lua/build_linux.go
+++ lua/build_linux.go
@@ -2,6 +2,5 @@
 
 package lua
 
-// #cgo CFLAGS: -I${SRCDIR}/../../../../../3rdparty/include
-// #cgo LDFLAGS: ${SRCDIR}/../../../../../3rdparty/lib/linux_amd64/liblua.a -lm -ldl
+// #cgo LDFLAGS: -llua -lm -ldl
 import "C"
